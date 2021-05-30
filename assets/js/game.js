be6f1352cbdf1c62dbf0e51b5b8acf57f5d416ca
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-05-30 16:19:40 -0400

    player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! Javascript is running");
+var playerName = window.prompt("What is your robot's name?");
+ 
+console.log(playerName) 
+// Note the lack of "" around player name^
+// console log is a good way to test code without interrupting the page
+// under Dev tools, check to see if it worked. also can see the file it is saved at and line number
+console.log("this logs a string, good for leavingyourselfa message");
+//this will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("Our robot's name is" +" "+  playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+//fight();
+
